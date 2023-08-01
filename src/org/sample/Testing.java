f3fea7b25195deada22389b042c44eec1c5a17e2
leelamanit <leelamani.t@gmail.com>
2023-08-01 20:51:56 +0530

    syed done his work

--- src/org/sample/Testing.java
+++ src/org/sample/Testing.java
@@ -2,6 +2,11 @@ package org.sample;
 
 public class Testing {
 
+	private void m2() {
+		
+		System.out.println("member 2");
+
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("hiii");
